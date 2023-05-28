3b4d47a4b77d9b5ff2b2e6ee4f6e95f296328a24
Eliseu <eliseusantico@gmail.com>
2023-05-28 19:43:02 -0300

    init commit

---
+++ src/main/java/br/com/wakandaclan/APIGestaoCotacaoFacil/ApiGestaoCotacaoFacilApplication.java
@@ -0,0 +1,13 @@
+package br.com.wakandaclan.APIGestaoCotacaoFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestaoCotacaoFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestaoCotacaoFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wakandaclan/APIGestaoCotacaoFacil/ApiGestaoCotacaoFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wakandaclan.APIGestaoCotacaoFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestaoCotacaoFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
