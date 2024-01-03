d93764a0bff09488757ac059bc4bad2dcfb41515
Eliseu Divino Santos <eliseusantico@gmail.com>
2024-01-03 19:53:10 -0300

    remove dependencia que n estava usando

--- src/test/java/br/com/wakandaclan/APIGestaoCotacaoFacil/fornecedor/application/api/service/FornecedorApplicationServiceTest.java
+++ src/test/java/br/com/wakandaclan/APIGestaoCotacaoFacil/fornecedor/application/api/service/FornecedorApplicationServiceTest.java
@@ -9,7 +9,6 @@ import br.com.wakandaclan.APIGestaoCotacaoFacil.fornecedor.application.api.repos
 import br.com.wakandaclan.APIGestaoCotacaoFacil.fornecedor.domain.entities.Fornecedor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
